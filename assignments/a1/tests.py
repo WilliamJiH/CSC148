618f9a9b3b28aa8a7d826f17765388704d76ace8
William ji <williamji12345@gmail.com>
2020-02-05 18:22:49 -0500

    Added own tests.py for testing

---
+++ assignments/a1/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from course import *
+from survey import *
+from criterion import *
+from grouper import *
+
+
+def test_student_has_answer():
+    pass
+
+
+def test_student_set_answer():
+    pass
+
+
+def test_student_get_answer():
+    pass
