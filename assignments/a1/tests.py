c1a078519b498946a5def769748f35ecaf281d6d
William ji <williamji12345@gmail.com>
2020-02-10 21:28:01 -0500

    Updated test methods

--- assignments/a1/tests.py
+++ assignments/a1/tests.py
@@ -5,13 +5,61 @@ from criterion import *
 from grouper import *
 
 
-def test_student_has_answer():
-    pass
+class Test_Student(unittest.TestCase):
+
+    def test_student_has_answer(self):
+        pass
+
+    def test_student_set_answer(self):
+        pass
+
+    def test_student_get_answer(self):
+        pass
 
 
-def test_student_set_answer():
-    pass
+class Test_Course(unittest.TestCase):
+
+    def test_course_enroll_students(self):
+        pass
+
+    def test_course_all_answered(self):
+        pass
+
+    def test_course_get_students(self):
+        pass
 
 
-def test_student_get_answer():
-    pass
+class Test_Question(unittest.TestCase):
+
+    def test_question_(self):
+        pass
+
+
+class Test_Survey(unittest.TestCase):
+
+    def test_survey_set_default_criterion(self):
+        pass
+
+    def test_survey_set_default_weight(self):
+        pass
+
+    def test_survey_get_question(self):
+        pass
+
+    def test_survey__get_criterion(self):
+        pass
+
+    def test_survey__get_weight(self):
+        pass
+
+    def test_survey_set_weight(self):
+        pass
+
+    def test_survey_set_criterion(self):
+        pass
+
+    def test_survey_score_students(self):
+        pass
+
+    def test_survey_score_grouping(self):
+        pass
